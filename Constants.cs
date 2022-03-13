c58f1aae30eb9ce5da658ca83bcb6f5da9c989ca
vites86@gmail.com <viktor.dubrovin@ukr.net>
2022-03-13 23:52:44 +0200

    Extract default port and method to Constants.

--- Constants.cs
+++ Constants.cs
@@ -5,5 +5,7 @@
         internal const string POWER_SHELL_PATH = @"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
         internal const string DB_PATH = @"target-history.txt";
         internal const string TARGET_TASK_PATH= @"IP.txt";
+        internal const int DEFAULT_PORT= 53;
+        internal const Method DEFAULT_METHOD = Method.udp;
     }
 }
